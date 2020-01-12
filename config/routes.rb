288249c2053d753548038de4518d4b3a00d98eab
kayo <kkworksmail2020@icloud.com>
2020-01-12 19:01:46 +0900

    toppageの実装
    完了の定義の実装ができたと思うのでご確認ください。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  # devise_for :users
   root "items#index"
+
+  # root "signup#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+resources :signup, only: [:index, :create] do
 end
+end
+
