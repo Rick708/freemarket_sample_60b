b4bfcc6ffca1f9fe7c917d104ae21c5df6090ab6
Rick708 <Rick708@users.noreply.github.com>
2020-01-19 20:02:28 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  # devise_for :users
   root "items#index"
-
-  # root "signup#index"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
-resources :signup, only: [:index, :create] do
-end
 end
 
