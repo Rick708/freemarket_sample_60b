1f1d934eeeb1a3f00785b77200c8b126966b146d
YourName <you@example.com>
2020-01-27 21:34:27 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root "items#addresses"
+  root "items#registration"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
