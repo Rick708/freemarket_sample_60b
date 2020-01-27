8a7fcbc39f9335b5c3c4f97e1056cff301ee86ae
kento.uoi <fishperson19@gmail.com>
2020-01-27 16:06:53 +0900

    register_credit1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root "items#index"
-  get 'login' => 'items#login'
-  get 'signin' => 'items#signin'
-  get 'details' => 'items#details'
-  get 'registration' => 'items#registration'
-  get 'verification' => 'items#verification'
-  get 'index' => 'mypage#index'
+  root "items#register_credit"
+  # get 'login' => 'items#login'
+  # get 'signin' => 'items#signin'
+  # get 'details' => 'items#details'
+  # get 'registration' => 'items#registration'
+  # get 'verification' => 'items#verification'
+  # get 'index' => 'mypage#index'
 end
