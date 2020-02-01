15c3dead26e6ff7e3a87ccb5035d784e897154fb
Rick708 <Rick708@users.noreply.github.com>
2020-02-01 14:02:39 +0900

    住所入力のcss変更

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get 'identification' => 'items#identification' #マイページの本人情報登録
   get 'tell' => 'items#tell' #新規登録の電話番号認証
   get 'doneview' => 'items#doneview' #新規登録の完了画面
-  get 'address' => 'items#address' ##新規登録の住所登録
+  get 'address' => 'items#address' ##新規登録の住所登録 mada
   get 'paymethod' => 'items#paymethod' ##新規登録のお支払い方法
   get 'register_credit' => 'items#register_credit' #マイページの支払い方法
   get 'new' => 'items#new' #商品出品ページ
