c6de2ee70e943814576a0011f73c5de0b8a4af3c
kento.uoi <fishperson19@gmail.com>
2020-02-03 00:08:55 +0900

    コンフリクト解消

--- db/schema.rb
+++ db/schema.rb
@@ -34,5 +34,18 @@ ActiveRecord::Schema.define(version: 2020_02_02_111725) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "images", "items"
 end
