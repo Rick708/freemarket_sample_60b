3a8c1bbd5d653cb148c8bd635e8014433954ac12
Rick708 <Rick708@users.noreply.github.com>
2020-02-05 23:11:34 +0900

    ログイン機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+
   def new
-    
+    @user = User.new(user_params)
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-
+  
 end
