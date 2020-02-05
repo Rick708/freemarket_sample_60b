0913f165c145a7ca5f58e0ac00569461712174a6
Rick708 <Rick708@users.noreply.github.com>
2020-02-06 00:15:22 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "items#index" #トップページ
-  # get 'login' => 'items#login' #ログインページ
+  get 'login' => 'items#login' #ログインページ
   get 'signin' => 'items#signin' #サインインページ
   # get 'details' => 'items#details' #商品詳細ページ
-  # post 'registration' => 'items#registration' #新規登録の会員情報入力
   # get 'verification' => 'items#verification' #購入内容確認
   # get 'logout' => 'items#logout' #マイページのログアウト
   # get 'identification' => 'items#identification' #マイページの本人情報登録
@@ -14,7 +13,7 @@ Rails.application.routes.draw do
   # get 'address' => 'items#address' ##新規登録の住所登録 mada
   # get 'paymethod' => 'items#paymethod' ##新規登録のお支払い方法
   # get 'register_credit' => 'items#register_credit' #マイページの支払い方法
-  # get 'new' => 'items#new' #商品出品ページ
+  get 'new' => 'items#new' #商品出品ページ
   # get 'profile' => 'mypages#profile' #マイページのプロフィール編集
   # get 'mypages' => 'mypages#index' #マイページ
 end
