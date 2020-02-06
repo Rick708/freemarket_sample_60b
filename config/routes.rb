9941267977f18f0e5e5e7439efb7fa1e95c32ee5
Git-kun <df.dropc24@gmail.com>
2020-02-06 23:31:39 +0900

    ルーティングコメントアウト解除

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,16 @@ Rails.application.routes.draw do
   root to: "items#index" #トップページ
   get 'login' => 'items#login' #ログインページ
   get 'signin' => 'items#signin' #サインインページ
-  # get 'details' => 'items#details' #商品詳細ページ
-  # get 'verification' => 'items#verification' #購入内容確認
-  # get 'logout' => 'items#logout' #マイページのログアウト
-  # get 'identification' => 'items#identification' #マイページの本人情報登録
-  # get 'tell' => 'items#tell' #新規登録の電話番号認証
-  # get 'doneview' => 'items#doneview' #新規登録の完了画面
-  # get 'address' => 'items#address' ##新規登録の住所登録 mada
-  # get 'paymethod' => 'items#paymethod' ##新規登録のお支払い方法
-  # get 'register_credit' => 'items#register_credit' #マイページの支払い方法
+  get 'details' => 'items#details' #商品詳細ページ
+  get 'verification' => 'items#verification' #購入内容確認
+  get 'logout' => 'items#logout' #マイページのログアウト
+  get 'identification' => 'items#identification' #マイページの本人情報登録
+  get 'tell' => 'items#tell' #新規登録の電話番号認証
+  get 'doneview' => 'items#doneview' #新規登録の完了画面
+  get 'address' => 'items#address' #新規登録の住所登録 
+  get 'paymethod' => 'items#paymethod' ##新規登録のお支払い方法
+  get 'register_credit' => 'items#register_credit' #マイページの支払い方法
   get 'new' => 'items#new' #商品出品ページ
-  # get 'profile' => 'mypages#profile' #マイページのプロフィール編集
-  # get 'mypages' => 'mypages#index' #マイページ
+  get 'profile' => 'mypages#profile' #マイページのプロフィール編集
+  get 'mypages' => 'mypages#index' #マイページ
 end
