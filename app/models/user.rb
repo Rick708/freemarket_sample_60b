91dc82ef030832cac28e86a5249e0db1f4ec7e47
Git-kun <df.dropc24@gmail.com>
2020-02-08 21:14:37 +0900

    gem 'factory_bot_rails'の導入
    テストコードを効率化する為のgem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname ,presence: true
+  validates :nickname ,presence: true, length: { maximum: 6 }
 end
