9f47ab542a318dcfeb30a181b40b5eb9554724a8
kento.uoi <fishperson19@gmail.com>
2020-02-14 18:22:54 +0900

    sell-item1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,16 @@
 class ItemsController < ApplicationController
-  
+  before_action :login_check, {only: [:new, :create]}
   def index
   end
 
   def new
     @item = Item.new
-    @item.images.build
   end
 
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to root_path, notice: '作成完了'
     else
       render :new
     end
@@ -29,11 +28,15 @@ class ItemsController < ApplicationController
   def destroy
   end
   
-
-  private
+  def login_check
+    unless user_signed_in?
+      redirect_to("/login")
+    end
+  end
+  
 
   def item_params
-    params.require(:item).permit(:name, :content, :price, :status, :delivery_charge, :send_day, :size, :delivery_method, :prefecture_code, item_images_attributes: [:image]).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :content, :price, :status, :delivery_charge, :send_day, :size, :delivery_method, :prefecture_code, item_images_attributes: [:image]).merge(seller_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  belongs_to :seller, class_name: "User"
+  belongs_to :buyer, class_name: "User"
   has_many :images
   accepts_nested_attributes_for :images
 end
