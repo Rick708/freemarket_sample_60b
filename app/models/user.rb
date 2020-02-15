18dfb9ef3cc86b932aaa9bca1e350e521ecbe1fb
Rick708 <Rick708@users.noreply.github.com>
2020-02-16 02:08:28 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname ,presence: true, length: { maximum: 6 }
+
+  validates :nickname ,presence: true
+
 end
