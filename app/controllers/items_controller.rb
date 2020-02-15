248c536194382c3e5dc9be898868e2c88ea850b6
kento.uoi <fishperson19@gmail.com>
2020-02-15 20:10:20 +0900

    sell-item2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,12 @@
 class ItemsController < ApplicationController
   before_action :login_check, {only: [:new, :create]}
   def index
+    @items = Item.includes(:images).order('created_at DESC')
   end
 
   def new
     @item = Item.new
+    @item.images.new
   end
 
   def create
@@ -36,7 +38,7 @@ class ItemsController < ApplicationController
   
 
   def item_params
-    params.require(:item).permit(:name, :content, :price, :status, :delivery_charge, :send_day, :size, :delivery_method, :prefecture_code, item_images_attributes: [:image]).merge(seller_id: current_user.id)
+    params.require(:item).permit(:name, :content, :price, :status, :delivery_charge, :send_day, :size, :delivery_method, :prefecture_code, :condition, images_attributes: [:image]).merge(seller_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :seller, class_name: "User"
-  belongs_to :buyer, class_name: "User"
+  belongs_to :buyer, class_name: "User", foreign_key: "user_id", optional: true
+
   has_many :images
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true
+
+  enum status: { 出品中: 0,取引中: 1,購入済み: 2}
 end
