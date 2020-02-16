543b7fb160b242d05c97a3737a756ec71937090f
Nico550363 <y5ut010@gmail.com>
2020-02-16 13:44:43 +0900

    ビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   
   def index
     @items = Item.includes(:images).limit(5).order("created_at DESC")
-    # @images = Image.limit(5).order("created_at DESC")
   end
 
   def new
