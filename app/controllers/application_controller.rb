66f562fb9431afb42448c623f5abe145fc0bce5e
kento.uoi <fishperson19@gmail.com>
2020-02-23 18:39:45 +0900

    ログインエラー解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   helper_method :current_user
-  before_action :authenticate_user!
 
   def index
   end
