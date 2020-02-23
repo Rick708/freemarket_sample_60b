d9377538418fe7aefa5687490474869964cce586
kento.uoi <fishperson19@gmail.com>
2020-02-23 18:09:39 +0900

    fakerエラー解消

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 
   factory :user do
-    password = Faker::Internet.password(min_length: 8)
+    
     nickname {"a"}
     email {Faker::Internet.free_email}
     password {password}
