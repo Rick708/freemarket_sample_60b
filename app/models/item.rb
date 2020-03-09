84aea6dc4418a0f7fff2c76fd87deb63147ea679
kento.uoi <fishperson19@gmail.com>
2020-03-09 13:43:46 +0900

    enum追加1

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,60 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :images, allow_destroy: true
 
 # enum設定
+  # 状態
   enum status: { exhibition: 0, buy: 1, sold: 2}
+  # 都道府県 
+  enum prefecture_code: {
+    hokkaido: 0,
+    aomori:   1,
+    iwate:    2,
+    miyagi:   3,
+    akita:    4,
+    yamagata: 5
+    # fukushima:
+    # ibaraki:
+    # tochigi:
+    # gunma:
+    # saitama:
+    # chiba:
+    # tokyo:
+    # kanagawa:
+    # niigata:
+    # toyama:
+    # ishikawa:
+    # fukui:
+    # yamanashi:
+    # nagano:
+    # gifu:
+    # shizuoka:
+    # aichi:
+    # mie:
+    # shiga:
+    # kyoto:
+    # osaka:
+    # hyogo:
+    # nara:
+    # wakayama:
+    # tottori:
+    # shimane:
+    # okayama:
+    # hiroshima:
+    # yamaguchi:
+    # tokushima:
+    # kagawa:
+    # ehime:
+    # kochi:
+    # fukuoka:
+    # saga:
+    # nagasaki:
+    # kumamoto:
+    # oita:
+    # miyazaki:
+    # kagoshima:
+    # okinawa:
+  }
+  # 配送料の負担 
+  enum delivery_charge: { include: 0, not: 1}
 
 # validation
   validates :price, presence: true
