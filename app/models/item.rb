b7108666e93bae2121cf673e5ad1a502602d76aa
Rick708 <Rick708@users.noreply.github.com>
2020-03-09 21:23:45 +0900

    enum

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,56 +11,56 @@ class Item < ApplicationRecord
   enum status: { exhibition: 0, buy: 1, sold: 2}
   # 都道府県 
   enum prefecture_code: {
-    hokkaido: 0,
-    aomori:   1,
-    iwate:    2,
-    miyagi:   3,
-    akita:    4,
-    yamagata: 5
-    # fukushima:
-    # ibaraki:
-    # tochigi:
-    # gunma:
-    # saitama:
-    # chiba:
-    # tokyo:
-    # kanagawa:
-    # niigata:
-    # toyama:
-    # ishikawa:
-    # fukui:
-    # yamanashi:
-    # nagano:
-    # gifu:
-    # shizuoka:
-    # aichi:
-    # mie:
-    # shiga:
-    # kyoto:
-    # osaka:
-    # hyogo:
-    # nara:
-    # wakayama:
-    # tottori:
-    # shimane:
-    # okayama:
-    # hiroshima:
-    # yamaguchi:
-    # tokushima:
-    # kagawa:
-    # ehime:
-    # kochi:
-    # fukuoka:
-    # saga:
-    # nagasaki:
-    # kumamoto:
-    # oita:
-    # miyazaki:
-    # kagoshima:
-    # okinawa:
+    "北海道":  0,
+    "青森県":  1,
+    "岩手県":  2,
+    "宮城県":  3,
+    "秋田県":  4,
+    "山形県":  5,
+    "福島県":  6,
+    "茨城県":  7,
+    "栃木県":  8,
+    "群馬県":  9,
+    "埼玉県":  10,
+    "千葉県":  11,
+    "東京都":  12,
+    "神奈川県":13,
+    "新潟県":  14,
+    "富山県":  15,
+    "石川県":  16,
+    "福井県":  17,
+    "山梨県":  18,
+    "長野県":  19,
+    "岐阜県":  20,
+    "静岡県":21,
+    "愛知県":22,
+    "三重県":23,
+    "滋賀県":24,
+    "京都府":25,
+    "大阪府":26,
+    "兵庫県":27,
+    "奈良県":28,
+    "和歌山県":29,
+    "鳥取県":30,
+    "島根県":31,
+    "岡山県":32,
+    "広島県":33,
+    "山口県":34,
+    "徳島県":35,
+    "香川県":36,
+    "愛媛県":37,
+    "高知県":38,
+    "福岡県":39,
+    "佐賀県":40,
+    "長崎県":41,
+    "熊本県":42,
+    "大分県":43,
+    "宮崎県":44,
+    "鹿児島県":45,
+    "沖縄県":46
   }
   # 配送料の負担 
-  enum delivery_charge: { include: 0, not: 1}
+  enum delivery_charge: { "送料込み(出品者負担)": 0, "着払い(購入者負担)": 1}
 
 # validation
   validates :price, presence: true
